d713adcc231c5147b38f747b930e0aa179620661
André Nascimento <lcn.andre@gmail.com>
2022-05-26 08:16:43 -0300

    Refactored directory structure to better follow clean architecture. Added basic middlewares.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpStatus } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from './application/modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Middlewares
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
+  app.use(helmet());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: true,
+    allowedHeaders: '*',
+    methods: '*',
+    credentials: true,
+    exposedHeaders: '*',
+    optionsSuccessStatus: HttpStatus.NO_CONTENT,
+  });
+
+  await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
