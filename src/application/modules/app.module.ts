f46d31451ec7a08746488ccfa781f17c3dc19437
André Nascimento <lcn.andre@gmail.com>
2022-05-26 09:58:46 -0300

    Initial db setup

--- src/application/modules/app.module.ts
+++ src/application/modules/app.module.ts
@@ -1,10 +1,18 @@
 /* istanbul ignore file */
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from '../../io/controllers/app.controller';
 import { AppService } from '../services/app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/domain/entities'],
+      entitiesTs: ['./src/domain/entities'],
+      dbName: 'questionnaire',
+      type: 'postgresql',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
